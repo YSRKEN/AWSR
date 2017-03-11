87cc738d2a36dfb4bf37687fec56a11ee0cbba68
YSRKEN <ysr.ken@gmail.com>
2017-03-12 07:54:20 +0900

    内部構造をMVVMらしく変更した (参考→http://yujiro15.net/YKSoftware/MVVM_Tree.html)

--- AWSR/App.xaml.cs
+++ AWSR/App.xaml.cs
@@ -8,10 +8,22 @@ using System.Windows;
 
 namespace AWSR
 {
-    /// <summary>
-    /// App.xaml の相互作用ロジック
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	using AWSR.Views;
+	using AWSR.ViewModels;
+	/// <summary>
+	/// App.xaml の相互作用ロジック
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			var w = new MainView();
+			var vm = new MainViewModel();
+
+			w.DataContext = vm;
+			w.Show();
+		}
+	}
 }
