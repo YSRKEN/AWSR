eb430469a23a923b8b7718abab656f2eb64a6a09
YSRKEN <ysr.ken@gmail.com>
2017-03-13 18:06:22 +0900

    敵艦隊の情報も表示できるようにした

---
+++ AWSR/Models/AirWarSimulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSR.Models
+{
+	static class AirWarSimulator
+	{
+		public enum Formation { Trail, SubTrail, Circle, Echelon, Abreast };
+		public static string ToStr(this Formation formation) {
+			string[] name = { "単縦陣", "複縦陣", "輪形陣", "梯形陣", "単横陣" };
+			return name[(int)formation];
+		}
+	}
+}
--- AWSR/Models/EnemyData.cs
+++ AWSR/Models/EnemyData.cs
@@ -1,15 +1,69 @@
-﻿namespace AWSR.Models
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using static AWSR.Models.AirWarSimulator;
+
+namespace AWSR.Models
 {
 	class EnemyData
 	{
+		// 各種定数
+
+		#region 分析用のモデル
+		[JsonObject("enemy")]
+		private class EnemyDataModel
+		{
+			// プロパティ
+			[JsonProperty("formation")]
+			private string formation { get; set; }
+			public Formation Formation {
+				get {
+					switch (formation) {
+					case "trail":
+						return Formation.Trail;
+					case "subTrail":
+						return Formation.SubTrail;
+					case "circle":
+						return Formation.Circle;
+					case "echelon":
+						return Formation.Echelon;
+					case "abreast":
+						return Formation.Abreast;
+					default:
+						return Formation.Trail;
+					}
+				}
+			}
+			[JsonProperty("fleet")]
+			public List<List<int>> Fleet { get; set; }
+		}
+		#endregion
+
+		/// <summary>
+		/// 入力されたJSON文字列を解析する
+		/// </summary>
+		/// <param name="inputEnemyDataText">敵艦隊用のJSON文字列</param>
+		/// <returns>デシリアライズした結果</returns>
+		private static EnemyDataModel ToEnemyDataModel(string inputEnemyDataText)
+			=> JsonConvert.DeserializeObject<EnemyDataModel>(inputEnemyDataText);
+
 		/// <summary>
 		/// 入力されたJSON文字列を解析し、敵艦隊文字列として出力する
 		/// </summary>
-		/// <param name="inputDeckBuilderText">敵艦隊用のJSON文字列</param>
+		/// <param name="inputEnemyDataText">敵艦隊用のJSON文字列</param>
 		/// <returns>敵艦隊文字列</returns>
-		public static string InfoText(string inputDeckBuilderText) {
-			string output = "スタブ";
-			// スタブ
+		public static string InfoText(string inputEnemyDataText) {
+			string output = "";
+			// JSONをデシリアライズする
+			var enemyData = ToEnemyDataModel(inputEnemyDataText);
+			// 艦隊毎に読み込み処理を行う
+			output += $"陣形 : {enemyData.Formation.ToStr()}\n";
+			foreach (var fleet in enemyData.Fleet.Select((v, i) => new { v, i })) {
+				output += $"第{fleet.i + 1}艦隊\n";
+				foreach (var ship in fleet.v.Select((v, i) => new { v, i })) {
+					output += $"　{ship.i + 1}番艦 : {ship.v}\n";
+				}
+			}
 			return output;
 		}
 	}
