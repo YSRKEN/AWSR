d156f7ee74ccf2bf360d64d77cb8bd2aa324e6bc
YSRKEN <ysr.ken@gmail.com>
2017-03-14 17:17:40 +0900

    KammusuDataクラスを作成

---
+++ AWSR/Models/Constant.cs
@@ -0,0 +1,25 @@
+﻿namespace AWSR.Models
+{
+	static class Constant
+	{
+		// 艦種
+		// ただし、浮遊要塞・護衛要塞・泊地棲鬼/姫・南方棲鬼は「重巡洋艦」、
+		// 南方棲戦鬼は「航空巡洋艦」、装甲空母鬼/姫・戦艦レ級は「航空戦艦」、
+		// 秋津洲は「水上機母艦」カテゴリに入れている
+		public enum FleetType {
+			None, PT, DD, CL, CLT, CA,
+			CAV, CVL, CC, BB, BBV, CV,
+			AF, SS, SSV, LST, AV, LHA,
+			ACV, AR, AS, CP, AO};
+		// 艦種を文字列に変換する
+		public static string ToStr(this FleetType fleetType)
+		{
+			string[] name = {
+				"無し", "魚雷艇", "駆逐艦", "軽巡洋艦", "重雷装巡洋艦", "重巡洋艦",
+				"航空巡洋艦", "軽空母", "巡洋戦艦", "戦艦", "航空戦艦", "正規空母",
+				"陸上型", "潜水艦", "潜水空母", "輸送艦", "水上機母艦", "揚陸艦",
+				"装甲空母", "工作艦", "潜水母艦", "練習巡洋艦", "給油艦" };
+			return name[(int)fleetType];
+		}
+	}
+}
---
+++ AWSR/Models/DataBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using static AWSR.Models.Constant;
+
+namespace AWSR.Models
+{
+	static class DataBase
+	{
+		private static Dictionary<int, KammusuData> kammusuDictionary;
+		// データベースを初期化
+		public static void Initialize()
+		{
+
+		}
+		// 艦娘データの内部表現
+		class KammusuData
+		{
+			public string Name { get; private set; }
+			public FleetType Type { get; private set; }
+			public int AntiAir { get; private set; }
+			public int[] Airs { get; private set; }
+			public int[] WeaponId { get; private set; }
+			public bool IsKammusu { get; private set; }
+			// コンストラクタ
+			public KammusuData(string name, FleetType type, int antiAir, int[] airs, int[] weaponId, bool isKammusu)
+			{
+				Name = name;
+				Type = type;
+				AntiAir = antiAir;
+				Airs = airs;
+				WeaponId = weaponId;
+				IsKammusu = isKammusu;
+			}
+		}
+	}
+}
