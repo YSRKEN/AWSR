3a2b1b437a446ec0acdb160b3b3d682fc89143bb
YSRKEN <ysr.ken@gmail.com>
2017-03-16 02:53:21 +0900

    制空値を計算し、制空判定結果を表示するようにした

--- AWSR/Models/Simulator.cs
+++ AWSR/Models/Simulator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AWSR.Models
 {
@@ -19,7 +20,33 @@ namespace AWSR.Models
 				// 状態を初期化する
 				CopyAirsList(firstFriendAirsList, friendAirsList);
 				CopyAirsList(firstEnemyAirsList, enemyAirsList);
+				// 制空値を計算する
+				int friendAirValue = NowAirValue(friend, friendAirsList);
+				int enemyAirValue = NowAirValue(enemy, enemyAirsList);
+				// 制空状態を判断し、結果を配列に加算する
+				if(friendAirValue >= enemyAirValue * 3) {
+					++AirWarStatusCount[0];
+				}else if(friendAirValue * 2 >= enemyAirValue * 3) {
+					++AirWarStatusCount[1];
+				}else if(friendAirValue * 3 >= enemyAirValue * 2) {
+					++AirWarStatusCount[2];
+				}else if(friendAirValue * 3 >= enemyAirValue) {
+					++AirWarStatusCount[3];
+				}
+				else {
+					++AirWarStatusCount[4];
+				}
 			}
+
+			// 結果を書き出す
+			output += "制空状態：\n";
+			output += "本隊";
+			output += $"　確保：{Math.Round(100.0 * AirWarStatusCount[0] / simulationSize, 1)}％";
+			output += $"　優勢：{Math.Round(100.0 * AirWarStatusCount[1] / simulationSize, 1)}％";
+			output += $"　均衡：{Math.Round(100.0 * AirWarStatusCount[2] / simulationSize, 1)}％";
+			output += $"　劣勢：{Math.Round(100.0 * AirWarStatusCount[3] / simulationSize, 1)}％";
+			output += $"　喪失：{Math.Round(100.0 * AirWarStatusCount[4] / simulationSize, 1)}％";
+			output += "\n";
 			return output;
 		}
 		// AirsListをコピーする
@@ -31,6 +58,20 @@ namespace AWSR.Models
 					}
 				}
 			}
+		}
+		// 現在の制空値を計算する
+		private static int NowAirValue(Fleet fleet, AirsList airslist) {
+			int airValue = 0;
+			for (int ui = 0; ui < fleet.Unit.Count; ++ui) {
+				var kammusuList = fleet.Unit[ui].Kammusu;
+				for (int ki = 0; ki < kammusuList.Count; ++ki) {
+					var weaponList = kammusuList[ki].Weapon;
+					for (int wi = 0; wi < weaponList.Count; ++wi) {
+						airValue += weaponList[wi].AirValue(airslist[ui][ki][wi]);
+					}
+				}
+			}
+			return airValue;
 		}
 	}
 }
